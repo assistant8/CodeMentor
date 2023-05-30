9138ac5918dc74f560bb95dd2d9f35bfdd4ba7a8
[SW4/2반] 정웅비 <zo0qzzz@gmail.com>
2023-05-30 15:45:06 +0900

    앱 구성 방식 파악 중

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MainLayout from "./components/layout/mainLayout";
 import LoginLayout from "./components/layout/loginLayout";
 import Quiz from "./pages/quiz";
 import QuizList from "./pages/quizList";
+import Login from "./pages/login";
 
 function App() {
   return (
@@ -20,7 +21,7 @@ function App() {
           <Route index element={<QuizList />} />
         </Route>
         <Route path={PATH.LOGIN} element={<LoginLayout />}>
-          <Route index element={<QuizList />} /> 
+          <Route index element={<Login />} />
           {/* 로그인 회원가입은 헤더 nav바 따로 없어서 레이아웃을 따로 만들었습니다 이쪽 구현 후  element에 넣어주시고 주석 지우시면 될것같습니다 */}
         </Route>
       </Routes>
---
+++ src/pages/login/index.js
@@ -0,0 +1,12 @@
+import "./login.scss";
+import Main from "../../components/login/Main";
+
+export default function Login() {
+  return (
+    <>
+      <SelectorWayToLogin />
+    </>
+  );
+}
+
+function WayToLogin() {}
