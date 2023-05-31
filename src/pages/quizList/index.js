206c78cb0918659ffbd7ea2469040364f4f29175
JoSooha <dlrhdcjs@naver.com>
2023-05-31 18:43:21 +0900

    design: 퀴즈리스트 검색창

--- src/pages/quizList/index.js
+++ src/pages/quizList/index.js
@@ -1,9 +1,16 @@
-import "./quizList.scss";
+import styles from "./quizList.module.scss";
 
 export default function QuizList() {
   return (
-    <div className="listContainer">
-        quizlist components default
+    <div className={styles.container}>
+      <div className={styles.searchContainer}>
+        <input
+          type="text"
+          className={styles.inputField}
+          placeholder="문제 제목 검색"
+        />
+        <button type="button" className={styles.searchButton}></button>
+      </div>
     </div>
-  )
+  );
 }
