7812739b260889e376c5f3ee7b95afe62ab8141b
[SW4/2반] 김나연 <apinksky00e@gmail.com>
2023-05-31 17:14:32 +0900

    feat: 비밀번호 수정 페이지 setState 구현

--- src/pages/mypage/ModifyUser.jsx
+++ src/pages/mypage/ModifyUser.jsx
@@ -1,16 +1,25 @@
 import styles from "./ModifyUser.module.scss";
 import { useNavigate } from "react-router-dom";
 import { VioletButton } from "../../components/buttons/VioletButton";
+import { useState } from "react";
 
 const ModifyUser = () => {
-  let navigate = useNavigate();
+  const navigate = useNavigate();
+  const [userName, setUserName] = useState("");
+  const onChange = (e) => {
+    setUserName(e.target.value);
+  };
   return (
     <div class={styles.modifyContainer}>
       <div class={styles.modifyImg}>
         <img src="" alt="프사" />
       </div>
       <div className={styles.inputBox}>
-        <input placeholder="유저_1B789RS" />
+        <input
+          placeholder="유저_1B789RS"
+          value={userName}
+          onChange={onChange}
+        />
         <p>중복된 유저명입니다</p>
       </div>
       <VioletButton>저장하기</VioletButton>
--- src/pages/mypage/Password.jsx
+++ src/pages/mypage/Password.jsx
@@ -1,7 +1,31 @@
 import styles from "./Password.module.scss";
 import { VioletButton } from "../../components/buttons/VioletButton";
+import { useState } from "react";
 
 const PassWord = () => {
+  const [newPwd, setNewPwd] = useState("");
+  const [checkPattern, setCheckPattern] = useState(false);
+  const [checkPwd, setCheckPwd] = useState(false);
+  const handlePwd = (e) => {
+    setNewPwd(e.target.value);
+    const pattern = /^(?=.*[a-z])(?=.*[A-Z]).{8,12}$/;
+    if (pattern.test(e.target.value)) {
+      setCheckPattern(true);
+    } else {
+      setCheckPattern(false);
+    }
+  };
+  const handleCheck = (e) => {
+    setCheckPwd(e.target.value);
+    if (e.target.value === newPwd) {
+      setCheckPwd(true);
+    } else {
+      setCheckPwd(false);
+    }
+  };
+  const onClick = () => {
+    console.log("제출됨");
+  };
   return (
     <div className={styles.pwdContainer}>
       <p className={styles.pwdTitle}>비밀번호 변경</p>
@@ -10,14 +34,16 @@ const PassWord = () => {
           <input placeholder="현재 비밀번호" />
         </div>
         <div className={styles.inputBox}>
-          <input placeholder="비밀번호" />
-          <p>영문 대소문자 포함 8~12자리</p>
+          <input placeholder="비밀번호" value={newPwd} onChange={handlePwd} />
+          {!checkPattern ? <p>영문 대소문자 포함 8~12자리</p> : null}
         </div>
         <div className={styles.inputBox}>
-          <input placeholder="비밀번호 확인" />
-          <p>비밀번호가 일치하지 않습니다</p>
+          <input placeholder="비밀번호 확인" onChange={handleCheck} />
+          {!checkPwd ? <p>비밀번호가 일치하지 않습니다</p> : null}
         </div>
-        <VioletButton style={{ marginTop: "40px" }}>변경하기</VioletButton>
+        <VioletButton style={{ marginTop: "40px" }} onClick={onClick}>
+          변경하기
+        </VioletButton>
       </div>
     </div>
   );
