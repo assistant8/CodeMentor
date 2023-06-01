7a138acc71a47f2f2a20db82313e5149953524cc
JoSooha <dlrhdcjs@naver.com>
2023-06-01 17:34:14 +0900

    design: userinput import 및 적용, userinput 수정

--- src/pages/quizList/index.jsx
+++ src/pages/quizList/index.jsx
@@ -1,16 +1,13 @@
 import styles from "./quizList.module.scss";
 import bookmark from "../../image/bookmark.png";
 import check from "../../image/check.png";
+import { UserInput } from "../../components/inputs/UserInput";
 
 export default function QuizList() {
   return (
     <div className={styles.container}>
       <div className={styles.searchContainer}>
-        <input
-          type="text"
-          className={styles.inputField}
-          placeholder="문제 제목 검색"
-        />
+        <UserInput placeholder="문제 제목 검색"/>
         <button type="button" className={styles.searchButton}></button>
       </div>
       <div className={styles.menuContainer}>
