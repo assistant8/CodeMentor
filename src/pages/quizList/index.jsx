00c340b81cd9ef62b9adfc09fbc5f0c17184b6c6
JoSooha <dlrhdcjs@naver.com>
2023-06-01 15:30:05 +0900

    design: categoryContainer 퍼블리싱

--- src/pages/quizList/index.jsx
+++ src/pages/quizList/index.jsx
@@ -1,4 +1,7 @@
 import styles from "./quizList.module.scss";
+import bookmark from "../../image/bookmark.png"
+import check from "../../image/check.png"
+
 
 export default function QuizList() {
   return (
@@ -8,10 +11,21 @@ export default function QuizList() {
           type="text"
           className={styles.inputField}
           placeholder="문제 제목 검색"
-        /> 
+        />
         <button type="button" className={styles.searchButton}></button>
       </div>
       <div className={styles.menuContainer}>
+        <div className={styles.categoryContainer}>
+          <ul>
+            <li>전체</li>
+            <li>백준</li>
+            <li>프로그래머스</li>
+          </ul>
+        </div>
+        <div className={styles.imageContainer}>
+          <img src={bookmark} alt="1" />
+          <img src={check} alt="2" />
+        </div>
       </div>
     </div>
   );
