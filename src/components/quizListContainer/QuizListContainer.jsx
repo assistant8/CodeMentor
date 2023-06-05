63ed2c17c672e3649a351a79c3be53101d2df268
JoSooha <dlrhdcjs@naver.com>
2023-06-05 22:48:09 +0900

    feat: quizlist map화 및 navigate 경로 수정

--- src/components/quizListContainer/QuizListContainer.jsx
+++ src/components/quizListContainer/QuizListContainer.jsx
@@ -1,23 +1,45 @@
-import styles from "./QuizListContainer.module.scss"
+import styles from "./QuizListContainer.module.scss";
 import bookmark from "../../image/bookmark.png";
 import check from "../../image/check.png";
 import { useNavigate } from "react-router-dom";
+import PATH from "../../constants/path";
 
-export const QuizListContainer = ({ style, isImgNeed=true }) => {
-    const navigate = useNavigate();
+export const QuizListContainer = ({ style, isImgNeed = true }) => {
+  const navigate = useNavigate();
+  const quizs = [
+    //더미
+    {
+      id: 1,
+      category: "그래프",
+      title: "토마토",
+    },
+    {
+      id: 2,
+      category: "구현",
+      title: "사과",
+    },
+  ];
+
   return (
     <div className={styles.quizListContainer}>
-      <div className={styles.quizList}>
-        <div className={styles.quizListTitle} onClick={() => navigate('/quiz')}>
-          백준 7596번 - 토마토가 이렇게 되지 않았는데
-        </div>
-        {isImgNeed && (
-          <div className={styles.imageContainer}>
-            <img src={bookmark} alt="bookmark" />
-            <img src={check} alt="check" />
+      {quizs.map((quiz) => {
+        return (
+          <div className={styles.quizList}>
+            <div
+              className={styles.quizListTitle}
+              onClick={() => navigate(PATH.QUIZ)} //navigate(PATH.QUIZ+`/:${quiz.id}`)
+            >
+              {quiz.title}
+            </div>
+            {isImgNeed && (
+              <div className={styles.imageContainer}>
+                <img src={bookmark} alt="bookmark" />
+                <img src={check} alt="check" />
+              </div>
+            )}
           </div>
-        )}
-      </div>
+        );
+      })}
     </div>
   );
 };
