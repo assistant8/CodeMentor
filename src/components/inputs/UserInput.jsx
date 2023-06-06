e9f471013fe2867f508d55cfb67d2d3ba393e614
[SW4/2반] 김나연 <apinksky00e@gmail.com>
2023-06-06 21:42:06 +0900

    CW-43 공용 컴포넌트 style props 제거

--- src/components/inputs/UserInput.jsx
+++ src/components/inputs/UserInput.jsx
@@ -2,12 +2,11 @@ import styles from "./UserInput.module.scss";
 import React from "react";
 
 export const UserInput = React.forwardRef(
-  ({ style, placeholder, value, onChange, type, name }, ref) => {
+  ({ placeholder, value, onChange, type, name }, ref) => {
     return (
       <input
         className={styles.UserInput}
         type={type}
-        style={style}
         placeholder={placeholder}
         value={value}
         onChange={onChange}
