85b3c3a6d79334ace9afdcb5f91da4261ad673c1
[SW4/2반] 정웅비 <zo0qzzz@gmail.com>
2023-06-06 15:59:20 +0900

    fix: UserInput 컴포넌트 ref 속성 추가.

--- src/components/inputs/UserInput.jsx
+++ src/components/inputs/UserInput.jsx
@@ -1,14 +1,18 @@
 import styles from "./UserInput.module.scss";
+import React from "react";
 
-export const UserInput = ({ style, placeholder, value, onChange, type }) => {
-  return (
-    <input
-      className={styles.UserInput}
-      type={type}
-      style={style}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-    ></input>
-  );
-};
+export const UserInput = React.forwardRef(
+  ({ style, placeholder, value, onChange, type }, ref) => {
+    return (
+      <input
+        className={styles.UserInput}
+        type={type}
+        style={style}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        ref={ref}
+      ></input>
+    );
+  }
+);
