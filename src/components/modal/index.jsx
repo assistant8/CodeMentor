b5b9f1c9645a7b00cce5259f792ee6d7713bfefe
[SW4/2반] 김나연 <apinksky00e@gmail.com>
2023-06-07 15:49:14 +0900

    fix: modal에 max-width 적용

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -1,6 +1,18 @@
 import styles from "./modal.module.scss";
 
 export const GradeModal = ({ isOpen, children }) => {
+  // 누르면 모달 띄우게 하려는 버튼 컴포넌트에서 아래 설정
+
+  //const [isOpen, setIsOpen] = useState(false);
+
+  //const openModal = () => {
+  //  setIsOpen(true);
+  //};
+
+  //const closeModal = () => {
+  //  setIsOpen(false);
+  //};
+
   if (!isOpen) return null;
 
   return (
