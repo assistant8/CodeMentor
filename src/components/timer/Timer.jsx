b8fbebb1ee42ae21f49a64b8282f5b8dc1db6b72
JoSooha <dlrhdcjs@naver.com>
2023-06-08 16:00:58 +0900

    feat: 타이머 일시정지 및 재시작 구현

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import timer from "../../image/timer.png"
 
 const Timer = ({ initialMinutes, initialSeconds, onComplete }) => { //초기값
   const [minutes, setMinutes] = useState(initialMinutes); //현재값 업뎃 용도
@@ -6,7 +7,8 @@ const Timer = ({ initialMinutes, initialSeconds, onComplete }) => { //초기값
   const [isPause, setIsPause] = useState(false);
 
   const handleTimerClick = () => { // 누르면 일시중지되거나 재생됨
-    setIsPause ? setIsPause(false) : setIsPause(true); //일시정지 표시 
+    isPause ? setIsPause(false) : setIsPause(true); //일시정지 표시 
+    console.log(isPause)
   }
 
   useEffect(() => {
@@ -14,7 +16,7 @@ const Timer = ({ initialMinutes, initialSeconds, onComplete }) => { //초기값
 
     const interval = setInterval(() => {
       totalSeconds--;
-      if (!isPause && totalSeconds > 0) {
+      if (!isPause && totalSeconds >= 0) {
         setMinutes(Math.floor(totalSeconds / 60));
         setSeconds(totalSeconds % 60);
       } else {
@@ -31,15 +33,19 @@ const Timer = ({ initialMinutes, initialSeconds, onComplete }) => { //초기값
       // 타이머가 0이 되었을 때 수행할 작업 - 부모에 끝났다고 말하고 부모에서 토스트 메시지 표출, 0:00에서 멈춰있도록 
       onComplete(); 
       setIsPause(true); //및 일시정지 표시 
+      console.log("끝")
     }
   }, [minutes, seconds]);
 
-  
 
   return (
-    <div onClick={handleTimerClick}>
-      <h1>Countdown Timer</h1>
-      <div>{`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`}</div>
+    <div onClick={handleTimerClick} style={{backgroundColor: 'yellow'}}>
+      <div style={{ position: 'relative', display: 'inline-block' }}>
+        <img src={timer} style={{ width: '100%' }} alt="Timer" />
+        <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+          {`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`}
+        </div>
+      </div>
     </div>
   );
 };
