01d8dd3c38d11dc72217c512eace6378d26b3481
JoSooha <dlrhdcjs@naver.com>
2023-06-10 15:50:00 +0900

    feat: 타이머 끝날때마다 각 단계의 토스트 메시지 구현 성공

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -1,25 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import timer from "../../image/timer.png"
 import styles from "./Timer.module.scss";
 
-
-const Timer = ({ initialMinutes, initialSeconds, onComplete }) => { //초기값
+//사용자 정지 버튼도 생성해야 , 토스트 모달창도 수정해야 
+const Timer = ({ initialMinutes, initialSeconds, onComplete, onStart }) => { //초기값
   const [minutes, setMinutes] = useState(initialMinutes); //현재값 업뎃 용도
   const [seconds, setSeconds] = useState(initialSeconds);
-  const [isPause, setIsPause] = useState(false);
+  const [isPause, setIsPause] = useState(true);
   const [isHover, setIsHover] = useState(false);
+  const [isAlive, setIsAlive] = useState(false); //0분0초 or 처음은 죽은걸로 간주, 일시정지는 true임
+
+  useEffect(()=>{
+    console.log("isAlive", isAlive)
+    console.log("isPause", isPause)
+    console.log("seconds", seconds)
+  }, [isPause])
+
+  useEffect(()=>{
+    setMinutes(initialMinutes);
+    setSeconds(initialSeconds);
+  }, [initialMinutes, initialSeconds])
 
   const handleTimerClick = () => { // 누르면 일시중지되거나 재생됨
     isPause ? setIsPause(false) : setIsPause(true); //일시정지 표시 
-    console.log(isPause)
+    if(!isAlive) {
+      onStart();
+      setIsAlive(true); //죽어있을 때(종료상태거나 처음) 터치하면 타이머 시작
+    }
   }
 
   useEffect(() => {
     let totalSeconds = minutes * 60 + seconds;
-
     const interval = setInterval(() => {
       totalSeconds--;
-      if (!isPause && totalSeconds >= 0) {
+      if (!isPause && isAlive && totalSeconds >= 0) {
         setMinutes(Math.floor(totalSeconds / 60));
         setSeconds(totalSeconds % 60);
       } else {
@@ -28,7 +42,7 @@ const Timer = ({ initialMinutes, initialSeconds, onComplete }) => { //초기값
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [minutes, seconds, isPause]); //m,s ?
+  }, [minutes, seconds, isPause, isAlive]); //m,s ?
 
 
   useEffect(() => {
@@ -36,6 +50,7 @@ const Timer = ({ initialMinutes, initialSeconds, onComplete }) => { //초기값
       // 타이머가 0이 되었을 때 수행할 작업 - 부모에 끝났다고 말하고 부모에서 토스트 메시지 표출, 0:00에서 멈춰있도록 
       onComplete(); 
       setIsPause(true); //및 일시정지 표시 
+      setIsAlive(false); //0:0 이면 죽었다 간주 
       console.log("끝")
     }
   }, [minutes, seconds]); //onComplete 넣으니까 3초에 한번 계속 불림
