806142ea5caf656e7de8a970d4216f48e78f5c91
김민성 <alstjd055@gmail.com>
2023-06-13 10:14:46 +0900

    [코드리뷰반영]: headerTitle RecoilValue에 담기

--- src/components/layout/mainLayout/index.js
+++ src/components/layout/mainLayout/index.js
@@ -3,12 +3,12 @@ import styles from "./MainLayout.module.scss";
 import { Outlet } from "react-router-dom";
 import Footer from "../footer/index"
 import Header from "../header/index";
-import { useRecoilState } from "recoil";
+import { useRecoilValue } from "recoil";
 import { headerTitleState } from "../../../state/headerTitleState";
 
 
 const MainLayout = ({children}) => {
-  const [headerTitle, setHeaderTitle] = useRecoilState(headerTitleState) //헤더로 전달안하고 헤더에서 직접 받아도 될 것
+  const headerTitle = useRecoilValue(headerTitleState) 
   
   return (
     <div className={styles.Full}>
