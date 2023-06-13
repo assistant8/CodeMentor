0db8e9a14fdb056179c19a58efc73ead870721fb
김민성 <alstjd055@gmail.com>
2023-06-13 10:56:37 +0900

    [코드리뷰반영]: header 코드 수정

--- src/components/layout/mainLayout/index.js
+++ src/components/layout/mainLayout/index.js
@@ -3,7 +3,6 @@ import styles from "./MainLayout.module.scss";
 import { Outlet } from "react-router-dom";
 import Footer from "../footer/index"
 import Header from "../header/index";
-import { useRecoilValue } from "recoil";
 
 const MainLayout = ({children}) => {
   // Header 컴포넌트에서 title 설정해놓음, 굳이 MainLayout에서 건들 필요 없어보임
