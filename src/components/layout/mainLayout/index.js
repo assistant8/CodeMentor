efc0c81bd3e227d01b603bd0da7cfbe892dc59fa
김민성 <alstjd055@gmail.com>
2023-06-13 10:21:33 +0900

    [코드리뷰반영]: MainLayout에서 RecoilValue 삭제

--- src/components/layout/mainLayout/index.js
+++ src/components/layout/mainLayout/index.js
@@ -4,15 +4,14 @@ import { Outlet } from "react-router-dom";
 import Footer from "../footer/index"
 import Header from "../header/index";
 import { useRecoilValue } from "recoil";
-import { headerTitleState } from "../../../state/headerTitleState";
-
 
 const MainLayout = ({children}) => {
-  const headerTitle = useRecoilValue(headerTitleState) 
+  // Header 컴포넌트에서 title 설정해놓음, 굳이 MainLayout에서 건들 필요 없어보임
+  // const headerTitle = useRecoilValue(headerTitle) 
   
   return (
     <div className={styles.Full}>
-        <Header headerTitle={headerTitle}/>
+        <Header />
         <div className={styles.Inner}>
             {children}
         </div>
