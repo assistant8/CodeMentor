f514c75cd62d6b938f2da30b745781f5cc332643
[SW4/2반] 김나연 <apinksky00e@gmail.com>
2023-06-15 12:25:26 +0900

    fix: 현재 비밀번호 확인 생략

--- src/state/userState.js
+++ src/state/userState.js
@@ -2,5 +2,16 @@ import { atom } from "recoil";
 
 export const userState = atom({
   key: "userState",
-  default: {},
+  default: {
+    id: null,
+    userName: "",
+    email: "",
+    image: null,
+    grade: "",
+    point: 0,
+    createdAt: "",
+    updatedAt: "",
+    isEmailVerified: false,
+    verificationCode: null,
+  },
 });
