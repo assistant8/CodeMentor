97f02bb79128e281f01da35f57085c64a227dd50
minseong <alstjd055@gmail.com>
2023-06-16 13:45:17 +0900

    힌트질문양식 통일

--- src/pages/admin/adminHintContainer.jsx
+++ src/pages/admin/adminHintContainer.jsx
@@ -2,12 +2,20 @@ import styles from './adminHintContainer.module.scss'
 import plusIcon from '../../image/plus.png'
 
 export default function AdminHintContainer({ hintLevel, hintContent, onChange, showImage=true, onClick }) {
+
+    const maplevelToQuestion = {
+        1: "문제 유형은 무엇인가요?", 
+        2: "세부 유형 또는 고려해야할 부분은 무엇일까요?",
+        3: "필수적으로 사용해야하는 것은 무엇인가요?",
+        4: "놓칠 수 있을만한 테스트 케이스는 무엇이 있을까요?",
+    }
+
     return (
         <div className={styles.hintContainer}>
             <div className={styles.hintName}>
                 <div className={styles.hintUpper}>
                     <div className={styles.hintTitle}>
-                    힌트 {hintLevel}
+                        {maplevelToQuestion[hintLevel]}
                     </div>
                     {/* 첫 번째 힌트 컨테이너에는 이미지가 보이지않게 하기위한 설정 */}
                     {showImage && <img src={plusIcon} alt="register" onClick={onClick}/>}
