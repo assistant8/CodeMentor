d306dab4454344effc8ba73bcb21d085fdcd6176
JoSooha <dlrhdcjs@naver.com>
2023-06-17 10:24:11 +0900

    fix: api 주소 수정

--- src/libs/utils/api.js
+++ src/libs/utils/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 // VM IP주소(34.64.81.88)로 변경
 axios.defaults.baseURL = "http://34.64.81.88/api";
-axios.defaults.baseURL = "http://34.64.81.88/api";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 // axios.defaults.baseURL = "http://localhost:3001/api";
